478e9506dc4e07896701eaac2b4e16d583506a2f
Kangjie <840416501@qq.com>
2018-12-13 20:09:01 +0800

    ok

--- app/src/main/java/com/kj/xptest/Test.java
+++ app/src/main/java/com/kj/xptest/Test.java
@@ -3,8 +3,9 @@ package com.kj.xptest;
 import android.util.Log;
 
 import de.robv.android.xposed.IXposedHookLoadPackage;
-import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XC_MethodReplacement;
 import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 import static de.robv.android.xposed.XposedHelpers.findAndHookMethod;
@@ -19,22 +20,67 @@ public class Test implements IXposedHookLoadPackage {
                 lpparam.packageName);
         Log.e("aaa","___________________________________________");
 
-        findAndHookMethod("com.meituan.banma.update.UpdateChecker", lpparam.classLoader, "b", String.class,
-                String.class, new XC_MethodHook() {
+     /*   findAndHookMethod("com.putius.picwall.ui.home.MomentDetailDelegate", lpparam.classLoader, "clickPause",  new XC_MethodHook() {
 
                     @Override
                     protected void beforeHookedMethod(MethodHookParam param) throws Throwable {
-                        XposedBridge.log("开始劫持了~");
-                        XposedBridge.log("参数1 = " + param.args[0]);
-                        XposedBridge.log("参数2 = " + param.args[1]);
+                        XposedBridge.log(new Gson().toJson(param.args[0])+"Loaded Test " +
+                                "app:_______________________________________________________________________________________________ " );
                     }
 
                     @Override
                     protected void afterHookedMethod(MethodHookParam param) throws Throwable {
-                        XposedBridge.log("劫持结束了~");
-                        XposedBridge.log("参数1 = " + param.args[0]);
-                        XposedBridge.log("参数2 = " + param.args[1]);
 
+
+                    }
+
+
+                });*/
+        /*findAndHookMethod("com.putius.picwall.ui.home.MomentDetailDelegate", lpparam.classLoader, "clickPause", new
+                XC_MethodReplacement() {
+
+            @Override
+            protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                try {
+                    return null;
+                    // 直接替换原来要执行的逻辑代码
+                } catch (Throwable t) {
+                    XposedBridge.log(t);
+                    return null;
+                }
+            }
+        });*/
+       /* final Class<?> ArgClass= XposedHelpers.findClass("com.putius.picwall.bean.MomentBean", lpparam.classLoader);
+        findAndHookMethod("com.putius.picwall.ui.home.MomentDetailDelegate", lpparam.classLoader, "setTime",ArgClass, new
+                XC_MethodReplacement() {
+
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                        try {
+                            return null;
+                            // 直接替换原来要执行的逻辑代码
+                        } catch (Throwable t) {
+                            XposedBridge.log(t);
+                            return null;
+                        }
+                    }
+                });*/
+
+        final Class<?> ArgClass= XposedHelpers.findClass("android.view", lpparam.classLoader);//android.os
+        final Class<?> ArgClass1= XposedHelpers.findClass("android.os.Bundle", lpparam.classLoader);//android.os
+
+        findAndHookMethod("com.meituan.passport.mtui.mobileLogin.AccountLoginFragment", lpparam.classLoader, "onViewCreated",ArgClass,ArgClass1, new
+                XC_MethodReplacement() {
+
+                    @Override
+                    protected Object replaceHookedMethod(MethodHookParam param) throws Throwable {
+                        try {
+                            return null;
+                            // 直接替换原来要执行的逻辑代码
+                        } catch (Throwable t) {
+                            XposedBridge.log(t);
+                            return null;
+                        }
                     }
                 });
     }
